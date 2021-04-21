641e1af1dda77c495e339f992d558e1cada65b68
ERBSON LAVRAS DOS SANTOS <erbson.lavras@globaltec.com.br>
2021-04-21 19:06:04 -0300

    novos imposto IHIT

--- IHIT.cs
+++ IHIT.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace Padrao
 {
-    class IHIT
+    public class IHIT : TemplateDeImpostoCondicional
     {
+        public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+        {
+
+            IList<String> noOrcamento = new List<String>();
+
+            foreach (Item item in orcamento.Itens)
+            {
+                if (noOrcamento.Contains(item.Nome))
+                    return true;
+                else
+                    noOrcamento.Add(item.getNome);
+            }
+
+            return false;
+
+
+
+        }
+
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.valor * 0.13 + 100;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.valor * (0.01 * orcamento.Itens.Count);
+        }
     }
 }
